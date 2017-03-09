691002c55e25084b833aaf775f8c76b5aa56b3be
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-03-09 13:37:47 +0900

    Divide HidAccessPropertyTypes.cs from HidAccessPropertyOptional.cs

---
+++ Windows/Projects/Egs/EgsDeviceControlCore/PropertyTypes/HidAccessPropertyOptional.cs
@@ -0,0 +1,67 @@
+﻿namespace Egs.PropertyTypes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Diagnostics;
+    using System.ComponentModel;
+    using System.Runtime.Serialization;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using Egs;
+    using Egs.EgsDeviceControlCore.Properties;
+    using Egs.DotNetUtility;
+
+    [DataContract]
+    public class HidAccessPropertyOptional<T> : HidAccessPropertyBase
+        where T : HidAccessPropertyOptionalTypeParameterBase, new()
+    {
+        [DataMember]
+        public OptionalValue<T> OptionalValue { get; set; }
+
+        public byte ValueOfSelectedItem
+        {
+            get { return OptionalValue.SelectedItem.Value; }
+            set
+            {
+                var hr = OptionalValue.SelectSingleItemByPredicate(e => e.Value == valueInByteArrayData);
+                if (hr)
+                {
+                    ByteArrayData[OneValueOffsetInByteArrayData] = ValueOfSelectedItem;
+                    RaiseValueUpdatedOnGetHidFeatureReport();
+                }
+            }
+        }
+
+        byte valueInByteArrayData { get { return ByteArrayData[OneValueOffsetInByteArrayData]; } }
+
+        internal override void RaiseValueUpdatedOnGetHidFeatureReport()
+        {
+            OptionalValue.SelectSingleItemByPredicate(e => e.Value == valueInByteArrayData);
+            // OnValueUpdated is called by the above line.
+        }
+
+        public HidAccessPropertyOptional()
+        {
+            OptionalValue = new OptionalValue<T>();
+        }
+
+        protected virtual void OnOptionalValueSelectedItemChanged(EventArgs e)
+        {
+            if (OptionalValue.Options.Count == 0) { return; }
+            if (OptionalValue.SelectedItem == null) { OptionalValue.SelectedIndex = 0; }
+            if (valueInByteArrayData != ValueOfSelectedItem)
+            {
+                ByteArrayData[OneValueOffsetInByteArrayData] = OptionalValue.SelectedItem.Value;
+                OnValueUpdated();
+            }
+        }
+
+        public void InitializeOnceAtStartup()
+        {
+            OptionalValue.SelectedItemChanged += (sender, e) => { OnOptionalValueSelectedItemChanged(e); };
+            OnOptionalValueSelectedItemChanged(EventArgs.Empty);
+        }
+    }
+}
--- Windows/Projects/Egs/EgsDeviceControlCore/PropertyTypes/HidAccessPropertyTypes.cs
+++ Windows/Projects/Egs/EgsDeviceControlCore/PropertyTypes/HidAccessPropertyTypes.cs
@@ -684,58 +684,6 @@
     }
 
     [DataContract]
-    public class HidAccessPropertyOptional<T> : HidAccessPropertyBase
-        where T : HidAccessPropertyOptionalTypeParameterBase, new()
-    {
-        [DataMember]
-        public OptionalValue<T> OptionalValue { get; set; }
-
-        public byte ValueOfSelectedItem
-        {
-            get { return OptionalValue.SelectedItem.Value; }
-            set
-            {
-                var hr = OptionalValue.SelectSingleItemByPredicate(e => e.Value == valueInByteArrayData);
-                if (hr)
-                {
-                    ByteArrayData[OneValueOffsetInByteArrayData] = ValueOfSelectedItem;
-                    RaiseValueUpdatedOnGetHidFeatureReport();
-                }
-            }
-        }
-
-        byte valueInByteArrayData { get { return ByteArrayData[OneValueOffsetInByteArrayData]; } }
-
-        internal override void RaiseValueUpdatedOnGetHidFeatureReport()
-        {
-            OptionalValue.SelectSingleItemByPredicate(e => e.Value == valueInByteArrayData);
-            // OnValueUpdated is called by the above line.
-        }
-
-        public HidAccessPropertyOptional()
-        {
-            OptionalValue = new OptionalValue<T>();
-        }
-
-        protected virtual void OnOptionalValueSelectedItemChanged(EventArgs e)
-        {
-            if (OptionalValue.Options.Count == 0) { return; }
-            if (OptionalValue.SelectedItem == null) { OptionalValue.SelectedIndex = 0; }
-            if (valueInByteArrayData != ValueOfSelectedItem)
-            {
-                ByteArrayData[OneValueOffsetInByteArrayData] = OptionalValue.SelectedItem.Value;
-                OnValueUpdated();
-            }
-        }
-
-        public void InitializeOnceAtStartup()
-        {
-            OptionalValue.SelectedItemChanged += (sender, e) => { OnOptionalValueSelectedItemChanged(e); };
-            OnOptionalValueSelectedItemChanged(EventArgs.Empty);
-        }
-    }
-
-    [DataContract]
     public class HidAccessPropertyRatioRect : HidAccessPropertyBase
     {
         [EditorBrowsable(EditorBrowsableState.Never)]
