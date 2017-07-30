0536735f6c699da5ac4b3f9a7aa36a2ffddf5989
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-30 20:43:47 +0900

    Remove unnecessary using from AssemblyInfo.cs files

--- Windows/Projects/Egs/ZkooHostApp/Properties/AssemblyInfo.cs
+++ Windows/Projects/Egs/ZkooHostApp/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following set of attributes. Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("ZkooHostApp")]
@@ -34,7 +32,6 @@ using System.Windows;
 [assembly: AssemblyFileVersion(Egs.ApplicationCommonSettings.ZkooHostAppExeAssemblyVersionMajorMinorBuildRevisionString)]
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
 
 // NOTE: set "uiAccess="false"" in Projects/Egs/ZkooHostApp/Properties/app.manifest
